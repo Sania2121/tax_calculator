70aa392339ef8bdbe15e2bb3068d95e91be3d83b
Sania <Saniachoudry@coop.co.uk>
2020-02-03 11:52:44 +0000

    made some tests pass for petrol vehicles

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -13,8 +13,32 @@ let TaxCalculator = class TaxCalculator {
   }
 
   calculateTax(vehicle) {
-    throw new Error('You have to implement the method doSomething!');
+    
+    if(vehicle.co2Emissions >= 1 && vehicle.co2Emissions <= 50) {
+      return 10
+    } else if(vehicle.co2Emissions >= 51 && vehicle.co2Emissions <= 75) {
+      return 25
+    } else if(vehicle.co2Emissions >= 76 && vehicle.co2Emissions <= 90) {
+      return 105
+    } else if(vehicle.co2Emissions >= 91 && vehicle.co2Emissions <= 100) {
+      return 125
+    } else if(vehicle.co2Emissions >= 101 && vehicle.co2Emissions <= 110) {
+      return 145
+    } else if(vehicle.co2Emissions >= 111 && vehicle.co2Emissions <= 130) {
+      return 165
+    } else if(vehicle.co2Emissions >= 131 && vehicle.co2Emissions <= 150) {
+      return 205
+    } else if(vehicle.co2Emissions >= 151 && vehicle.co2Emissions <= 170) {
+      return 515
+    } else {
+      return 0
+    }
   }
 }
 
 module.exports = { TaxCalculator: TaxCalculator }
+
+// 171 - 190	£830
+// 191 - 225	£1,240
+// 226 - 255	£1,760
+// Over 255	£2,070
